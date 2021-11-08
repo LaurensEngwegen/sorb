ba214420a2250e6a963771cbefbf0275ae78eacc
Laurens Engwegen <laurensengwegen@gmail.com>
2021-11-08 09:55:56 +0100

    added MaxDist experiment and changed code

--- experiments.py
+++ experiments.py
@@ -11,52 +11,87 @@ from graphsearch import *
 from tqdm import tqdm
 import numpy as np
 
-def distance_exp(eval_tf_env, agent, search_policy, goal_distance, max_duration):
-    seed = np.random.randint(0, 1000000)
-    
-    eval_tf_env.pyenv.envs[0]._duration = max_duration
-    eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
-        prob_constraint=1.0,
-        min_dist=goal_distance-1,
-        max_dist=goal_distance+1)
-
-    search = [1, 0]
-    steps = [0, 0]
-    for use_search in search:
-        np.random.seed(seed)
-        ts = eval_tf_env.reset()
-        step_counter = 0
-        for _ in range(eval_tf_env.pyenv.envs[0]._duration):
-            if ts.is_last():
-                break
-            if use_search:
-                action = search_policy.action(ts)
-            else:
-                action = agent.policy.action(ts)
-            ts = eval_tf_env.step(action)
-            step_counter += 1
-        if step_counter < eval_tf_env.pyenv.envs[0]._duration:
-            steps[use_search] = step_counter
-
+def rollout(seed, eval_tf_env, agent, search_policy, use_search=1):
+    np.random.seed(seed)
+    ts = eval_tf_env.reset()
+    step_counter = 0
+    for _ in range(eval_tf_env.pyenv.envs[0]._duration):
+        if ts.is_last():
+            break
+        if use_search:
+            action = search_policy.action(ts)
+        else:
+            action = agent.policy.action(ts)
+        ts = eval_tf_env.step(action)
+        step_counter += 1
+    if step_counter < eval_tf_env.pyenv.envs[0]._duration:
+        steps = step_counter
+    else:
+        steps = 0
     return steps
 
-def print_success(search_steps, nosearch_steps, n_experiments):
-    print(f'\nSEARCH')
-    if len(search_steps) > 0:
-        print(f'\tSuccess rate: {len(search_steps)/n_experiments}')
-        print(f'\tAverage number of steps to reach goal: {sum(search_steps)/len(search_steps)}')
-    else:
-        print(f'Success rate: 0.0')
-    print(f'\nNO SEARCH')
-    if len(nosearch_steps) > 0:
-        print(f'\tSuccess rate: {len(nosearch_steps)/n_experiments}')
-        print(f'\tAverage number of steps to reach goal: {sum(nosearch_steps)/len(nosearch_steps)}\n')
+def print_results(title, n_steps, n_experiments):
+    print(f'\n{title}')
+    successes = np.count_nonzero(n_steps)
+    if successes > 0:
+        rate = successes/n_experiments
+        print(f'\tSuccess rate: {rate}')
+        print(f'\tAverage number of steps to reach goal: {sum(n_steps)/successes}')
     else:
         print(f'Success rate: 0.0')
 
-environments = ['FourRooms', 'Maze6x6']
 
-for env_name in environments:
+
+def distance_exp(n_experiments, max_duration):
+    tf.compat.v1.reset_default_graph()
+
+    max_episode_steps = 20
+    resize_factor = 5 # Inflate the environment to increase the difficulty.
+
+    tf_env = env_load_fn(env_name, max_episode_steps, resize_factor=resize_factor, terminate_on_timeout=False)
+    eval_tf_env = env_load_fn(env_name, max_episode_steps, resize_factor=resize_factor, terminate_on_timeout=True)
+
+    agent = UvfAgent(
+            tf_env.time_step_spec(),
+            tf_env.action_spec(),
+            max_episode_steps=max_episode_steps,
+            use_distributional_rl=True,
+            ensemble_size=3)
+    
+    train_eval(
+			agent,
+			tf_env,
+			eval_tf_env,
+			initial_collect_steps=1000,
+			eval_interval=1000,
+			num_eval_episodes=10,
+			num_iterations=100000,
+	)
+
+    # Initialize search policy
+    replay_buffer_size = 1000
+    rb_vec = fill_replay_buffer(eval_tf_env, replay_buffer_size=replay_buffer_size)
+    agent.initialize_search(rb_vec, max_search_steps=7)
+    search_policy = SearchPolicy(agent, rb_vec, open_loop=True)
+
+    distances = [10, 20, 40, 60]
+    for distance in distances:
+        print(f'\tDistance set to {distance}')
+        seed = np.random.randint(0, 1000000)
+        eval_tf_env.pyenv.envs[0]._duration = max_duration
+        eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
+            prob_constraint=1.0,
+            min_dist=distance,
+            max_dist=distance)
+        search = [1, 0]
+        steps = [[], []]
+        for _ in tqdm(range(n_experiments)):
+            for use_search in search:
+                steps[use_search].append(rollout(seed, eval_tf_env, agent, search_policy, use_search))
+        print_results('SEARCH', steps[1], n_experiments)
+        print_results('NO SEARCH', steps[0], n_experiments)
+
+def maxdist_exp(n_experiments, max_duration):
     tf.compat.v1.reset_default_graph()
 
     max_episode_steps = 20
@@ -79,27 +114,39 @@ for env_name in environments:
 			initial_collect_steps=1000,
 			eval_interval=1000,
 			num_eval_episodes=10,
-			num_iterations=30000,
+			num_iterations=100000,
 	)
 
-    # Initialize search policy
-    replay_buffer_size = 1000
-    rb_vec = fill_replay_buffer(eval_tf_env, replay_buffer_size=replay_buffer_size)
-    agent.initialize_search(rb_vec, max_search_steps=7)
-    search_policy = SearchPolicy(agent, rb_vec, open_loop=True)
-
-    print(f'Starting experiments in environment: {env_name}')
-
-    max_duration = 300
-    n_experiments = 100
-    goal_distances = [10, 20, 40, 60]
+    max_dists = [5, 7, 11, 13, 15]
+    steps_results = {dist: [] for dist in max_dists}
     
-    for goal_distance in goal_distances:
-        search_steps = []
-        nosearch_steps = []
-        print(f'{n_experiments} experiments, with max. {max_duration} timesteps and distance to goal = {goal_distance}')
-        for _ in tqdm(range(n_experiments)):
-            steps = distance_exp(eval_tf_env, agent, search_policy, goal_distance, max_duration)
-            search_steps.append(steps[1])
-            nosearch_steps.append(steps[0])
-        print_success(search_steps, nosearch_steps, n_experiments)
+    print(f'\tExperiments with MaxDists: {max_dists}')
+
+    for _ in tqdm(range(n_experiments)):
+        seed = np.random.randint(0, 1000000)
+        eval_tf_env.pyenv.envs[0]._duration = max_duration
+        eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
+            prob_constraint=1.0,
+            min_dist=10,
+            max_dist=60)
+        for max_dist in max_dists:
+            # Initialize search policy
+            replay_buffer_size = 1000
+            rb_vec = fill_replay_buffer(eval_tf_env, replay_buffer_size=replay_buffer_size)
+            agent.initialize_search(rb_vec, max_search_steps=max_dist)
+            search_policy = SearchPolicy(agent, rb_vec, open_loop=True)
+            steps_results[max_dist].append(rollout(seed, eval_tf_env, agent, search_policy))
+    for dist in max_dists:
+        print_results(f'MaxDist = {dist}', steps_results[dist], n_experiments)
+
+
+
+n_experiments = 100
+max_duration = 300
+
+environments = ['FourRooms', 'Maze6x6']
+
+for env_name in environments:
+    print(f'Starting experiments in environment: {env_name}')
+    distance_exp(n_experiments, max_duration)
+    maxdist_exp(n_experiments, max_duration)
