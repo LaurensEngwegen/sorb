87b150dc5858cde4ef35eb2c5e4f8ac478475af7
Laurens Engwegen <laurensengwegen@gmail.com>
2022-01-24 20:56:36 +0100

    fixed plot function for multiple repititions of experiment

--- plot.py
+++ plot.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle as pkl
 
-def get_success_rates(results, conditions):
+def get_success_rates(results):
+    '''
     # Will contain success rate (for each of the labels: a list with for every condition the success rate)
     successes = []
     # Calculate success rates and store them in successes
@@ -25,12 +26,32 @@ def get_success_rates(results, conditions):
                     successes[i].append(cond_successes/n_experiments)
                 else:
                     successes[i].append(0)
-    return successes, n_experiments
+    '''
 
-def plot_successrate(results, env, conditions, xlabel):
-    success_rates, n_experiments = get_success_rates(results, conditions)
-    
-    labels = [key for key in results.keys()]
+    # {'search': {}, no_search: {}}
+    # {30: {search: [], nosearch: []}, 60: {}}
+
+    successes = {}
+
+    for param_value, conditions in results.items():
+        successes[param_value] = {}
+        for condition, steps in conditions.items():
+            n_experiments = len(results[param_value][condition])
+            n_successes = np.count_nonzero(results[param_value][condition])
+            if n_successes > 0:
+                successes[param_value][condition] = n_successes/n_experiments
+            else:
+                successes[param_value][condition] = 0
+
+
+    return successes
+
+def plot_successrate(success_rates, env, labels, conditions, xlabel):
+
+
+    avg_sr = np.average(success_rates, 0)
+    std_sr = np.std(success_rates, 0)
+
     x = [] # Used for the label locations
     x.append(np.arange(len(labels)))
     width = 0.15  # The width of the bars
@@ -40,7 +61,7 @@ def plot_successrate(results, env, conditions, xlabel):
     for i in range(len(conditions)):
         if i>0:
             x.append([j+width for j in x[i-1]])
-        ax.bar(x[i], success_rates[i], width, label=conditions[i])
+        ax.bar(x[i], avg_sr[:,i], width=width, label=conditions[i])
     ax.set_ylim([0,1])
     ax.set_ylabel('Success rate')
     ax.set_xlabel(xlabel)
@@ -73,7 +94,7 @@ def print_avg_steps(results, conditions):
                 print(f'Average number of steps to reach goal: {n_steps/successes}')
 
 def plot_experiments(experiments, env, resize_factor, training_iters):
-    plot_args = {
+    xlabels = {
         'distance': {'conditions': ['no search', 'search'], 'xlabel': 'Distance to goal'}, 
         'kmeansdistance': {'conditions': ['default', 'k-means'], 'xlabel': 'Distance to goal'},
         'kmeansbuffersize': {'conditions': ['default', 'k-means'], 'xlabel': 'Replay buffer size'},
@@ -81,9 +102,23 @@ def plot_experiments(experiments, env, resize_factor, training_iters):
         'upsampling': {'conditions': ['1', '5', '10', '50', '100'], 'xlabel': 'Replay buffer size'}
         }
     for exp in experiments:
-        with open(f'results/results_{exp}_{env}_resize{resize_factor}_trainiters{int(training_iters/1000)}k.pkl', 'rb') as f:
-            results = pkl.load(f)
-        plot_successrate(results, env, **plot_args[exp])
+
+        all_succ_rates = [[],[],[]]
+
+        for i in range(1, 4):
+            with open(f'results/results_{exp}_{env}_run{i}.pkl', 'rb') as f:
+                results = pkl.load(f)
+
+            success_rates = get_success_rates(results)
+
+            param_values = [key for key in results]
+            conditions = [key for key in results[param_values[0]]]
+            for j, param in enumerate(param_values):
+                all_succ_rates[i-1].append([])
+                for k, condition in enumerate(conditions):
+                    all_succ_rates[i-1][j].append(success_rates[param][condition])
+
+        plot_successrate(all_succ_rates, env, param_values, conditions, xlabels[exp]['xlabel'])
         # print(f'\nCounting Nones for experiment "{exp}" in environment {env}')
         # print_nones(results, plot_args[exp]['conditions'])
         print_avg_steps(results, plot_args[exp]['conditions'])
@@ -92,7 +127,7 @@ envs = ['FourRooms']
 resize_factor = 10
 training_iters = 1000000
 # Possible experiments: 'distance', 'kmeansdistance', 'kmeansbuffersize', 'maxdist', 'upsampling'
-experiments = ['upsampling']
+experiments = ['maxdist']
 
 for env in envs:
     plot_experiments(experiments, env, resize_factor, training_iters)
