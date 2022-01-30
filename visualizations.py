72487a4b9e2fdbeb8b8d46ab5129ae7bdc92c7f1
Laurens Engwegen <laurensengwegen@gmail.com>
2022-01-31 00:46:18 +0100

    updated and introduced new visualizations

--- visualizations.py
+++ visualizations.py
@@ -1,3 +1,5 @@
+from graphsearch import fill_replay_buffer, SearchPolicy
+
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
@@ -34,33 +36,7 @@ def get_rollout(tf_env, policy, seed=None):
 	waypoint_vec = np.array(waypoint_vec)
 	return obs_vec, goal, waypoint_vec
 
-def visualize_naive_rollouts(eval_tf_env, agent):
-	eval_tf_env.pyenv.envs[0]._duration = 100	# We'll give the agent lots of time to try to find the goal.
-	difficulty = 0.7
-	max_goal_dist = eval_tf_env.pyenv.envs[0].gym.max_goal_dist
-	eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
-			prob_constraint=1.0,
-			min_dist=max(0, max_goal_dist * (difficulty - 0.05)),
-			max_dist=max_goal_dist * (difficulty + 0.05))
-	plt.figure(figsize=(8, 4))
-	for col_index in range(2):
-		plt.subplot(1, 2, col_index + 1)
-		plot_walls(eval_tf_env.pyenv.envs[0].env.walls)
-		obs_vec, goal, _ = get_rollout(eval_tf_env, agent.policy)
-
-		plt.plot(obs_vec[:, 0], obs_vec[:, 1], 'b-o', alpha=0.3)
-		plt.scatter([obs_vec[0, 0]], [obs_vec[0, 1]], marker='+',
-								color='red', s=200, label='start')
-		plt.scatter([obs_vec[-1, 0]], [obs_vec[-1, 1]], marker='+',
-								color='green', s=200, label='end')
-		plt.scatter([goal[0]], [goal[1]], marker='*',
-								color='green', s=200, label='goal')
-		if col_index == 0:
-			plt.legend(loc='lower left', bbox_to_anchor=(0.3, 1), ncol=3, fontsize=16)
-	plt.show()
-
-def visualize_rollouts(eval_tf_env, agent, search_policy, rb_vec, distance=None):
-	eval_tf_env.pyenv.envs[0]._duration = 300
+def visualize_search_vs_nosearch(eval_tf_env, agent, search_policy, rb_vec, distance=None):
 	seed = np.random.randint(0, 1000000)
 
 	if distance is None:
@@ -120,4 +96,87 @@ def visualize_rollouts(eval_tf_env, agent, search_policy, rb_vec, distance=None)
 
 			plt.plot(waypoint_vec[:, 0], waypoint_vec[:, 1], 'y-s', alpha=0.3, label='waypoint')
 			plt.legend(loc='lower left', bbox_to_anchor=(-0.8, -0.15), ncol=4, fontsize=16)
+	plt.show()
+
+def visualize_start_goal(seed, eval_tf_env):
+	eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
+		prob_constraint=1.0,
+		min_dist=60,
+		max_dist=60)
+
+	np.random.seed(seed)
+	
+	ts = eval_tf_env.reset()
+	goal = ts.observation['goal'].numpy()[0]
+	start = ts.observation['observation'].numpy()[0]
+
+	plt.figure(figsize=(12,9))
+	plot_walls(eval_tf_env.pyenv.envs[0].env.walls)
+	plt.scatter([start[0]], [start[1]], marker='+', color='red', s=200, label='start')
+	plt.scatter([goal[0]], [goal[1]], marker='*', color='green', s=200, label='goal')
+	plt.show()
+
+def visualize_replaybuffer(seed, eval_tf_env, kmeans):
+	eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
+		prob_constraint=1.0,
+		min_dist=60,
+		max_dist=60)
+
+	np.random.seed(seed)
+	ts = eval_tf_env.reset()
+	goal = ts.observation['goal'].numpy()[0]
+	start = ts.observation['observation'].numpy()[0]
+	rb_vec = fill_replay_buffer(eval_tf_env, use_kmeans=kmeans)
+
+	plt.figure(figsize=(12,9))
+	plot_walls(eval_tf_env.pyenv.envs[0].env.walls)
+	plt.scatter([start[0]], [start[1]], marker='+', color='red', s=200, label='start')
+	plt.scatter([goal[0]], [goal[1]], marker='*', color='green', s=200, label='goal')
+	plt.scatter(*rb_vec.T, alpha=0.5)
+	plt.show()
+
+def visualize_search_path(seed, eval_tf_env, agent, kmeans):
+
+	rb_vec = fill_replay_buffer(eval_tf_env, use_kmeans=kmeans)
+	agent.initialize_search(rb_vec, max_search_steps=8)
+	search_policy = SearchPolicy(agent, rb_vec, open_loop=True)
+
+	eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
+		prob_constraint=1.0,
+		min_dist=60,
+		max_dist=60)
+
+	np.random.seed(seed)
+	ts = eval_tf_env.reset()
+	goal = ts.observation['goal'].numpy()[0]
+	start = ts.observation['observation'].numpy()[0]
+	obs_vec = []
+	for _ in range(eval_tf_env.pyenv.envs[0]._duration):
+		if ts.is_last():
+			break
+		obs_vec.append(ts.observation['observation'].numpy()[0])
+		# if use_search:
+		try:
+			action = search_policy.action(ts)
+		except:
+			# print('Error: no path found from start to goal')
+			break
+		# else:
+		# 	action = agent.policy.action(ts)
+		ts = eval_tf_env.step(action)
+	obs_vec = np.array(obs_vec)
+
+	# Get waypoints
+	waypoint_vec = [start]
+	for waypoint_index in search_policy._waypoint_vec:
+		waypoint_vec.append(rb_vec[waypoint_index])
+	waypoint_vec.append(goal)
+	waypoint_vec = np.array(waypoint_vec)
+
+	plt.figure(figsize=(12,9))
+	plot_walls(eval_tf_env.pyenv.envs[0].env.walls)
+	plt.scatter([start[0]], [start[1]], marker='+', color='red', s=200, label='start')
+	plt.scatter([goal[0]], [goal[1]], marker='*', color='green', s=200, label='goal')
+	plt.plot(waypoint_vec[:, 0], waypoint_vec[:, 1], 'y-s', alpha=0.3, label='waypoint')
+	plt.legend(loc='lower left', bbox_to_anchor=(-0.8, -0.15), ncol=4, fontsize=16)
 	plt.show()
